de8d0c95a159210356e7e5c71d340a099ad4d5d0
Cassio Moreira <cassio.moreira@hp.com>
2021-01-06 01:51:35 +0000

    terraform-asg initial version

---
+++ modules/terraform-aws-asg/output.tf
@@ -0,0 +1,11 @@
+output "launch-configuration-name" {
+  value = aws_launch_configuration.moreira-lab.name
+}
+
+output "asg-name" {
+  value = aws_autoscaling_group.moreira-lab.name
+}
+
+output "my_own_ip" {
+  value = data.http.my_own_public_ip.body
+}
---
+++ modules/terraform-aws-asg/variable.tf
@@ -0,0 +1,88 @@
+variable "name" {
+  default = "moreira-lab"
+  type = string
+  description = "The name of resource"
+}
+
+variable "image_id" {
+  default = ""
+  type = string
+  description = "The image of your instance"
+}
+
+variable "tags" {
+  default     = {}
+  type        = map(string)
+  description = "Map containing tags to associate to AWS resources."
+}
+
+variable "ebs_optimized" {
+  default = true
+  type = bool
+  description = "Definition if ebs volume is true or false"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+  type = string
+  description = "Define instance type"
+}
+
+variable "key_name" {
+  default = "moreira-lab"
+  type = string
+  description = "The key used to access the instance using ssh"
+}
+
+variable "user_data" {
+  default = null
+  type = string
+  description = "An script to run at boot of instance. You can customize values and set default things you want run at launch of instance"
+}
+
+variable "enable_monitoring" {
+  default = true
+  type = bool
+  description = "This value enables a lot of monitoring on aws side, you can see this values in cloudwatch"
+}
+
+variable "root_block_device" {
+  default = [{
+    volume_type           = "gp2"
+    volume_size           = 8
+    encrypted             = true
+    iops                  = 0
+    delete_on_termination = true
+  }]
+
+  type = list(
+  object(
+  {
+    volume_type           = string,
+    volume_size           = number,
+    encrypted             = bool,
+    iops                  = number,
+    delete_on_termination = bool
+  }
+  )
+  )
+  description = "Customize details about the root block device of the instance."
+}
+
+variable "ebs_block_device" {
+  default     = []
+
+  type = list(
+  object(
+  {
+    volume_type           = string,
+    volume_size           = number,
+    encrypted             = bool,
+    iops                  = number,
+    delete_on_termination = bool,
+    device_name           = string
+  }
+  )
+  )
+  description = "Additional EBS block devices to attach to the instance."
+}
