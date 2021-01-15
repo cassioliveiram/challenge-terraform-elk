65f58b4246990394e7c03a291f154e1ffdd80ba3
Cassio Moreira <cassio.moreira@hp.com>
2021-01-15 06:29:48 +0000

    organizando a estrutura de pastas

--- packer-aws-image/provisioners/scripts/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-sudo rm /var/lib/apt/lists/* > /dev/null 2>&1
-
-echo "[INFO] Starting update..."
-sudo apt-get update > /dev/null 2>&1
-echo "[INFO] Update complete..."
--- terraform-modules/terraform-aws-asg/user-data.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin
-
-wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.1.1-amd64.deb
-sudo dpkg -i filebeat-7.1.1-amd64.deb
